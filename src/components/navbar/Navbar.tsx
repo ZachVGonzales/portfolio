fb7dbc0cfa91d417970b14d3043133ebdf3c4dc1
ZachVGonzales <ZachVGonzales@gmail.com>
2024-12-04 14:34:31 -0800

    inital home page

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from "./navbar.module.css";
+import Link from "next/link";
+
+
+export default function Navbar() {
+  return (
+    <header>
+        <nav className={style.navbar}>
+        <ul className={style.navList}>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/gallery">Gallery</Link></li>
+            <li><Link href="/projects">Projects</Link></li>
+            <li><Link href="/resume">Resume</Link></li>
+        </ul>
+        </nav>
+
+    </header>
+  );
+}
